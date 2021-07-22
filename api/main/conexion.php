ef774b4890cf3c735521ff192b9da5d81c675451
Marirozz <64573125+Marirozz@users.noreply.github.com>
2021-07-21 22:45:20 -0400

    Validacion de informacion en campos de formulario

--- api/main/conexion.php
+++ api/main/conexion.php
@@ -1,7 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     // $con = new mysqli("localhost", "id15911985_root", "M@riana301300", "id15911985_webpack");
-    $con = new mysqli("localhost", "root", "", "sistema");
+    $con = new mysqli("localhost", "root", "mariana3013", "talleres_lebron");
     if ($con->connect_errno) {
         die("La conexion con el servidor no pudo establecerse");
     }
